54e13631cb764405b4eb4410d5c001545cd0c404
Gebruiker <douwe.raverss@gmail.com>
2022-10-02 23:52:40 +0200

    final source before deadline.

--- Assets/Scripts/CutPart.cs
+++ Assets/Scripts/CutPart.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class CutPart : MonoBehaviour
 {
+    [SerializeField]
+    AudioSource _cutSound;
     public void Tick()
     {
         if (CodeManager.Instance.Memory[3] == 0) return;
@@ -9,6 +11,7 @@ public class CutPart : MonoBehaviour
         if (Physics.Raycast(transform.position + Vector3.up * 0.5f, transform.forward * 5, out RaycastHit hit, 5))
         {
             if (hit.collider.gameObject.layer != 6) return;
+            _cutSound.Play();
             hit.collider.GetComponent<Animation>().Play();
             CodeManager.Instance.Memory[3] = 0;
             CodeManager.Instance.Memory[4]++;
